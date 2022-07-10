50b4b204705e8bfabc54b48c30709143b94e6765
Igor <guselnikov.uniikm@yandex.ru>
2022-07-10 18:34:13 +0500

    second task

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] array = new int [10];
+int sumUnevenElements = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99,100);
+    if (i%2 == 1)
+    {
+        sumUnevenElements += array[i];
+    }
+}
+Console.WriteLine($"Выведем сформированный массив: [{String.Join(", ",array)}], сумма элементов с нечентыми значениями индексов массива равно: {sumUnevenElements}");
